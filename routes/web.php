51dc53f9ef8d5405903353c29ca66f4db0b8f326
JohnIanOngayi <ongayijohnian@gmail.com>
2025-06-29 15:18:47 +0300

    Blade layouts and routes
    - Implemented named routes
    - Implemented a layout in app.blade.php
    - Used {{ route() }} directive in blade

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view(uri: '/', view: 'home')->name(name: 'home');
 
-Route::view('/second', 'second');
+Route::view(uri: '/second', view: 'second')->name(name: 'second');
+
+Route::view(uri: '/contact', view: 'contact')->name(name: 'contact');
+
+Route::view(uri: '/about', view: 'about')->name(name: 'about');
