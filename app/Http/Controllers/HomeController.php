9db1d2dc61f23c2a026636bc1e076167932deb3d
JohnIanOngayi <ongayijohnian@gmail.com>
2025-06-29 16:11:28 +0300

    Added Eloquent model `Category` linked to table `categories`

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $allCategories = DB::table(table: 'categories')->get();
+        // $allCategories = DB::table(table: 'categories')->get();
+        $allCategories = Category::all();
         return view(view: 'home', mergeData: ['categories' => $allCategories]);
     }
 }
